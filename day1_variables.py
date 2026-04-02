d081ae6c4eacedf61745b24d67cdcb6492409337
StephenRaju3 <stephenrajuchokka@gmail.com>
2026-04-02 10:02:42 +0530

    Create day1_variables.py

---
+++ day1_variables.py
@@ -0,0 +1,18 @@
+# Day 1 - Variables & Data Types
+# Cybersecurity target scanner
+
+attacker = "stephen"
+target_ip = "192.168.12.1"
+port1 = 80
+port2 = 443
+port3 = 143
+vuln_score = 7.9
+online = False
+
+print("Attacker:", attacker)
+print("Target IP:", target_ip)
+print("Open Port 1:", port1)
+print("Open Port 2:", port2)
+print("Open Port 3:", port3)
+print("Vulnerability Score:", vuln_score)
+print("Target Online:", online)
